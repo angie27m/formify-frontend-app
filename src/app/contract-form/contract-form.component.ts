54989d8db2cf4cd0c6aadf6de02950f5ce63a046
Angie Manrique <angie27manrique@gmail.com>
2024-09-23 16:55:36 -0500

    Initial project setup

---
+++ src/app/contract-form/contract-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './contract-form.component.html',
+  styleUrl: './contract-form.component.sass'
+})
+export class ContractFormComponent {
+
+}
